1589398fc53ec34bb8388a1b5ee0ac7103bb9679
blanchekebila-star <blanchekebila@gmail.com>
2026-04-01 22:26:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
 const cacheName = 'jsl-v1';
 const assets = ['./', './index.html', './manifest.json'];
 
+// Installation rapide
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
+// Activation immédiate
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+// Réponse ultra-rapide (Priorité au cache)
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
